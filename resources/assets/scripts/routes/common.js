4fe55cbb83a9260f3c75e34a2968102b426d7007
oplichko <oplichko@splitdev.pro>
2019-11-07 19:05:03 +0200

    7.11.19 end

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,4 +1,4 @@
-
+import Forms from './forms'
 
 export default {
   init() {
@@ -25,6 +25,41 @@ export default {
       $('.nav__mobile-button').toggleClass('open-mobile');
       $('.mobile-menu').toggleClass('active-menu');
     });
+
+    $('.main-form__glass').click(function (e) {
+      e.preventDefault();
+      let parent = $(this).parent();
+      let attrib = parent.children('.main-form__repass').attr('type');
+      if ('password' == attrib) {
+        parent.children('.main-form__repass').attr('type','text');
+      } else {
+        parent.children('.main-form__repass').attr('type','password');
+      }
+    });
+
+    $('.form-open').click(function (e) {
+      e.preventDefault();
+      $('.main-form').addClass('main-form-open');
+    });
+    $('.main-form__close-position a').click(function (e) {
+      e.preventDefault();
+      $('.main-form').removeClass('main-form-open');
+    });
+
+    $('.search-open').click(function (e) {
+      e.preventDefault();
+      $('.main-search').addClass('main-search-open');
+    });
+    $('.main-search__close-position a').click(function (e) {
+      e.preventDefault();
+      $('.main-search').removeClass('main-search-open');
+    });
+
+    const form = new Forms({
+      wrap: '.field-wrap',
+      forms: 'form.main-form__regist',
+    });
+    form.init();
   },
   // JavaScript to be fired on all pages, after page specific JS is fired
   finalize() {
