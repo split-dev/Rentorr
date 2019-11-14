d6322ec98c22c2244acea62e0a0ce4b3ddbff90f
oplichko <oplichko@splitdev.pro>
2019-11-14 15:28:20 +0200

    14.11.19 bugs fixed

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -2,20 +2,18 @@
 // require('intersection-observer');
 
 // import external dependencies
+import './util/polyfills'
 import 'jquery';
-
-// Import everything from autoload
-import './autoload/**/*'
 
 // import local dependencies
 import Router from './util/Router';
 import common from './routes/common';
-// import Detection from './util/Detection';
+import Detection from './util/Detection';
 
-// window._detector = new Detection({
-//   detect: ['ie11'],
-// })
-// window._detector.init()
+window._detector = new Detection({
+  detect: ['ie11'],
+});
+window._detector.init();
 
 // /** Populate Router instance with DOM routes */
 const routes = new Router({
