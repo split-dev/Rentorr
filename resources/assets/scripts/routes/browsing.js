cd0b3f24dc1a728455f9b2e93d595ed173e8a8e5
oplichko <oplichko@splitdev.pro>
2019-12-02 18:06:04 +0200

    2.12.19

--- resources/assets/scripts/routes/browsing.js
+++ resources/assets/scripts/routes/browsing.js
@@ -1,5 +1,7 @@
 import 'slick-carousel';
 import Forms from './forms';
+import '@chenfengyuan/datepicker';
+import 'ion-rangeslider';
 
 export default {
   init() {
@@ -73,6 +75,7 @@ export default {
     // filters on browsing.html
 
      $('.filters-menu__items ul li').click(function () {
+       $('.filters-menu__items ul li div').removeClass('filters-menu__dropdown-active')
       if($(this).children('.filters-menu__dropdown').hasClass('filters-menu__dropdown-active')) {
         // eslint-disable-next-line no-unused-vars
         let test;
@@ -83,14 +86,82 @@ export default {
       }
     });
     $(document).mouseup(function (e){
-      if($('.filters-menu__dropdown').hasClass('filters-menu__dropdown-active')) {
-        var div = $('.filters-menu__dropdown');
-        if (!div.is(e.target) && div.has(e.target).length === 0) {
-          $('.filters-menu__dropdown').removeClass('filters-menu__dropdown-active');
-          $('.filters-menu__items ul li a').removeClass('filters-menu__filter-color-active');
+      if ($('.datepicker-container').hasClass('datepicker-hide')) {
+        if($('.filters-menu__dropdown').hasClass('filters-menu__dropdown-active')) {
+          var div = $('.filters-menu__dropdown');
+          if (!div.is(e.target) && div.has(e.target).length === 0) {
+            $('.filters-menu__dropdown').removeClass('filters-menu__dropdown-active');
+            $('.filters-menu__items ul li a').removeClass('filters-menu__filter-color-active');
+          }
         }
       }
     });
+    // fitters plus-minus
+    $('.filters-menu__rooms-position .filters-menu__rooms-minus').click(function () {
+      let elementMinus = $(this).parent().children('.filters-menu__rooms-input');
+      let minusLink = $(this).parent().children('.filters-menu__rooms-minus');
+      let value = Number(elementMinus.attr('data-value'));
+      let dataMin = Number(elementMinus.attr('data-min'));
+      let plusLink = $(this).parent().children('.filters-menu__rooms-plus');
+      let dataMax = Number(elementMinus.attr('data-max'));
+      if (value == dataMin) {
+        minusLink.addClass('rooms-minus-disabled');
+      } else {
+        minusLink.removeClass('rooms-minus-disabled');
+        let step = Number(elementMinus.attr('data-step'));
+        let value = Number(elementMinus.attr('data-value'));
+        value = value - step;
+        elementMinus.attr('value', value + '+');
+        elementMinus.attr('data-value', value );
+        if (value == dataMin) {
+          minusLink.addClass('rooms-minus-disabled');
+        }
+        if (value == dataMax) {
+          plusLink.addClass('rooms-minus-disabled');
+        } else {
+          plusLink.removeClass('rooms-minus-disabled');
+        }
+      }
+    });
+    $('.filters-menu__rooms-position .filters-menu__rooms-plus').click(function () {
+      let elementPlus = $(this).parent().children('.filters-menu__rooms-input');
+      let plusLink = $(this).parent().children('.filters-menu__rooms-plus');
+      let value = Number(elementPlus.attr('data-value'));
+      let dataMax = Number(elementPlus.attr('data-max'));
+      let minusLink = $(this).parent().children('.filters-menu__rooms-minus');
+      let dataMin = Number(elementPlus.attr('data-min'));
+      if (value == dataMax) {
+        plusLink.addClass('rooms-minus-disabled');
+      } else {
+        plusLink.removeClass('rooms-minus-disabled');
+        let step = Number(elementPlus.attr('data-step'));
+        let value = Number(elementPlus.attr('data-value'));
+        value = value + step;
+        elementPlus.attr('value', value + '+');
+        elementPlus.attr('data-value', value );
+        if (value == dataMin) {
+          minusLink.addClass('rooms-minus-disabled');
+        } else {
+          minusLink.removeClass('rooms-minus-disabled');
+        }
+        if (value == dataMax) {
+          plusLink.addClass('rooms-minus-disabled');
+        }
+      }
+    });
+    //calendar
+    $('[data-toggle="datepicker"]').datepicker( {
+      autoShow: true,
+      autoHide: true,
+    });
+    //range-slider
+
+    $('.js-range-slider').ionRangeSlider({
+      type: 'double',
+      min: 1,
+      max: 24,
+    });
+
     const form = new Forms({
       wrap: '.field-wrap',
       forms: '.main-form__filters, filters-menu__input-position',
