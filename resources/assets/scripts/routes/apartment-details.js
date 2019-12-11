f240751ac358b0820c716fe7aed4209aa2fd7059
RuslanSplit <rpazenko@splitdev.pro>
2019-12-11 16:19:20 +0200

    11.12.2019 button close in slider

--- resources/assets/scripts/routes/apartment-details.js
+++ resources/assets/scripts/routes/apartment-details.js
@@ -11,9 +11,12 @@ export default {
     });
     $('#aniimated-thumbnials').lightGallery({
       thumbnail:true,
+      animateThumb: false,
+      showThumbByDefault: true,
+      thumbMargin: 20,
       mode: 'lg-fade',
-      thumbContHeight: 200,
-      thumbWidth: 200,
+      thumbContHeight: 120,
+      thumbWidth: 120,
     });
   },
   // JavaScript to be fired on all pages, after page specific JS is fired
