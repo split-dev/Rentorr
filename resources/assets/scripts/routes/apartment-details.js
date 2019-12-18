0a27120118b4611ae6fd6a21356fd4e517bf2462
RuslanSplit <rpazenko@splitdev.pro>
2019-12-18 15:35:35 +0200

    18.12.2019 header static and swipe gallery on small screens

--- resources/assets/scripts/routes/apartment-details.js
+++ resources/assets/scripts/routes/apartment-details.js
@@ -9,12 +9,26 @@ export default {
       slidesToShow: 1,
       slidesToScroll: 1,
     });
-    $('.apartment-details-map__slider2').slick({
+    $('.apartment-details-map__gallery2').slick({
       dots: true,
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
       swipe: true,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
     });
     $('#aniimated-thumbnials').lightGallery({
       thumbnail:true,
@@ -26,6 +40,18 @@ export default {
       thumbContHeight: 120,
       thumbWidth: 120,
     });
+    function stickyElement() {
+      var header = document.querySelector('.header');
+      var headerHeight = getComputedStyle(header).height.split('px')[0];
+      var navbar = document.querySelector('.navigation');
+      var scrollValue = window.scrollY;
+      if (scrollValue > headerHeight){
+        navbar.classList.add('is-fixed');
+      } else if (scrollValue < headerHeight){
+        navbar.classList.remove('is-fixed');
+      }
+    }
+    window.addEventListener('scroll', stickyElement);
   },
   // JavaScript to be fired on all pages, after page specific JS is fired
   finalize() {
